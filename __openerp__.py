4d24380c937c075e009b75c06ea3c7e4f2fd9855
Mikolaj Dziurzynski <mikolaj.dziurzynski@openglobe.pl>
2015-02-03 11:28:23 +0100

    Module portal_invitation. Issue #63

---
+++ __openerp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    portal_invitation
+#    (C) 2015 big-consulting GmbH
+#    (C) 2015 OpenGlobe
+#    Author: Thorsten Vocks (openBIG.org)
+#    Author: Mikołaj Dziurzyński, Grzegorz Grzelak (OpenGlobe)
+#
+#    All Rights reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Portal Invitation from Template",
+    "version": "0.01 (8.0)",
+    "author": "openbig.org",
+    "website": "http://www.openbig.org",
+    "category": "CRM",
+    "description": """""",
+    "depends": [
+        'email_template',
+        'portal',
+    ],
+    "demo_xml": [],
+    "data": [
+        'data/email_template_data.xml',
+        ],
+    "active": False,
+    "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
