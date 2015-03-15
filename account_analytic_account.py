674215f94b93ea674172b39ec1d3da592c8d09b3
Mikolaj Dziurzynski <mikolaj.dziurzynski@openglobe.pl>
2015-03-15 15:59:30 +0100

    Issue #181 kanban view of contracts

--- account_analytic_account.py
+++ account_analytic_account.py
@@ -23,27 +23,11 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{
-    "name": "Portal Customization for Customers",
-    "version": "0.01 (8.0)",
-    "author": "openbig.org",
-    "website": "http://www.openbig.org",
-    "category": "CRM",
-    "description": """ This module introduces portal customization for customers. Throught the portal, customers can:\n *view their quotations, sale orders and invoices""",
-    "depends": [
-        'project_sla',
-        'crm_contractmanagement',
-        'portal_sale',
-        'web_kanban',
-    ],
-    "demo_xml": [],
-    "data": [
-        'account_analytic_account_view.xml',
-        'security/ir.model.access.csv',
-        'security/portal_security.xml',
-        'views/contract_view.xml',
-    ],
-    "active": False,
-    "installable": True
-}
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+from openerp.osv import fields, osv
+
+class account_analytic_account(osv.Model):
+	_inherit = 'account.analytic.account'
+	_columns = {
+		'color': fields.integer('Color Index'),
+	}
